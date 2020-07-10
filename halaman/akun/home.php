a71df9619d3af35da771d276e42da534cd97d199
Hendra Saleh <36198221+hendrasaleh@users.noreply.github.com>
2020-07-10 19:32:18 +0700

    Edit

--- halaman/akun/home.php
+++ halaman/akun/home.php
@@ -1,85 +1,256 @@
-<?php 
-// include "function.php";
-// include "database.php";
+<?php
 
 $data = new database();
-
-$query = $data->getDb()->query("SELECT * FROM siswa WHERE user_name = $username");
-$hasil = $query->fetchAll();
-
-foreach ($hasil as $key) {
+$data_siswa = $data->getDb()->query("SELECT * FROM siswa WHERE user_name = '$username'");
+$siswa = $data_siswa->fetchAll();
+//print_r($siswa);
+foreach ($siswa as $s) {
     # code...
-    $pranama = $key['nama_lengkap'];
-    $nama = htmlentities($pranama,ENT_QUOTES);
-    $kelas = $key['kelas'];
-    $no_absen = $key['indeks'];
-    $r_kelas = strtolower(str_replace("-", "", $kelas));
-    $r_no = $no_absen < 10 ? "0".$no_absen : $no_absen;
+    $user_name = $s['user_name'];
+    $nama_lengkap = $s['nama_lengkap'];
+    $level = $s['level'];
+    $kelas = $s['kelas'];
 }
+$data_mapel = $data->getDb()->query("SELECT * FROM mapel WHERE level = $level");
+                $mapel = $data_mapel->fetchAll();
+                //print_r($mapel);
 ?>
-	<section id="inner-headline">
-	<div class="container">
-		<div class="row">
-			<div class="col-lg-12">
-				<h2 class="pageTitle">E - REPORT | <?= $nama." | ".$r_kelas; ?></h2>
-			</div>
-		</div>
-	</div>
-	</section>
-	<section id="content">
-		<div class="container content">		
-        <!-- Service Blcoks -->
-			
-			<div class="row"> 
-				<div class="col-md-12">
-					<h3>Alhamdulillah PAT Online Telah Selesai</h3>
-                    <p>Selamat kepada seluruh santri MTs Husnul Khotimah 2 Kuningan yang insyaAllah dirahmati Allah SWT. Alhamdulillah, Penilaian Akhir Tahun (PAT) Online tahun ajaran 2019/2020 telah selesai dilaksanakan. </p>
-                    <p>Ini adalah halaman untuk mengakses raport secara daring. Silahkan klik menu di bawah untuk mengunduh raport pondok dan kemenag.</p>
-                    <p>Semoga Allah SWT memberkahi kita semua dan semoga mendapatkan hasil terbaik.</p>
-
-				</div>
-			</div>
-            <div class="row">
-                <div class="skill-home">
-                    <div class="skill-home-solid clearfix">
-                        <div class="col-md-6 text-center">
-                            <span class="icons c1"><i class="fa fa-book"></i></span>
-                            <div class="box-area">
-                                <h3>RAPORT PONDOK</h3> <p>Silahkan klik tombol di bawah ini untuk mengunduh Raport Pondok dalam bentuk PDF.</p>
-                                <a href="<?= "raport/".$r_kelas."-".$r_no."-p.pdf" ?>" target="_blank" class="btn btn-medium">
-                                    <i class="icon-bolt"></i> UNDUH
-                                </a>
-                            </div>
+        <!-- About us section start -->
+        <div class="section primary-section" id="about">
+            <div class="triangle"></div>
+            <div class="container">
+                <div class="title">
+                    <h1>E - Learning</h1>
+                    <p>Pembelajaran Daring Melalui Video Pembelajaran, Rangkuman Materi Dan Tes Pehahaman Materi.</p>
+                </div>
+                <div class="row-fluid team">
+                    <?php
+                        foreach ($mapel as $m) :
+                        $kode = $m['kode'];
+                        $materi = $data->tampilMateribyMapel($kode);      
+                    ?>
+                    <div class="span3">
+                        <div class="thumbnail">
+                            <img src="<?= $m['gambar']; ?>" img="images">
+                            <h3><?= $m['nama']; ?></h3>
+                            <select name="id_mater" id="cars">
+                            <option value="-">Pilih Materi</option>
+                            <?php                        
+                                foreach ($materi as $mat) {
+                                    echo "<option value=".$mat['id'].">";
+                                    echo $mat['judul'];
+                                    echo "</option>";
+                                }
+                            ?>
+                            </select>
+                            <p>
+                                <label for="cars"><a href="user.php?page=materi&id=<?= $mat['id']; ?>"><button  class="btn btn-medium"> Pilih Materi</button></a></label>
+                            </p>
                         </div>
-                        <div class="col-md-6 text-center"> 
-                            <span class="icons c2"><i class="fa fa-trophy"></i></span>
-                            <div class="box-area">
-                                <h3>RAPORT KEMENAG</h3>
-                                <p>Silahkan klik tombol di bawah ini untuk mengunduh Raport Kemenag dalam bentuk PDF.</p>
-                                <p><a href="<?= "raport/".$r_kelas."-".$r_no."-k.pdf" ?>" target="_blank" class="btn btn-medium"><i class="icon-bolt"></i> UNDUH</a></p>
-                            </div>
+                    </div>
+                    <?php
+                        endforeach;
+                    ?>
+                    <!--<div class="span3" id="second-person">
+                        <div class="thumbnail">
+                            <img src="images/mapel/ipa.png" img="images">
+                            <h3>IPA</h3>
+                            <label for="cars"><button  class="btn btn-medium"> Pilih Materi</button></label>
+                            <select name="cars" id="cars">
+                            <option value="volvo">Volvo</option>
+                            <option value="saab">Saab</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="span3" id="third-person">
+                        <div class="thumbnail">
+                            <img src="images/mapel/fiqih.png" img="images">
+                            <h3>Fiqih</h3>
+                            <label for="cars"><button  class="btn btn-medium"> Pilih Materi</button></label>
+                            <select name="cars" id="cars">
+                            <option value="volvo">Volvo</option>
+                            <option value="saab">Saab</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="span3" id="third-person">
+                        <div class="thumbnail">
+                            <img src="images/mapel/khot-imla.png" img="images">
+                            <h3>Khot-Imla</h3>
+                            <label for="cars"><button  class="btn btn-medium"> Pilih Materi</button></label>
+                            <select name="cars" id="cars">
+                            <option value="volvo">Volvo</option>
+                            <option value="saab">Saab</option>
+                            </select>
+                        </div>
+                    </div>-->
+                </div>
+                <!--</br>
+                <div class="row-fluid team">
+                    <div class="span3" id="first-person">
+                        <div class="thumbnail">
+                            <img src="images/mapel/tafsir.png" img="images">
+                            <h3>tafsir</h3>
+                            <label for="cars"><button  class="btn btn-medium"> Tafsir</button></label>
+                            <select name="cars" id="cars">
+                            <option value="volvo">Volvo</option>
+                            <option value="saab">Saab</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="span3" id="second-person">
+                        <div class="thumbnail">
+                            <img src="images/mapel/matematika.png" img="images">
+                            <h3>Matematika</h3>
+                            <label for="cars"><button  class="btn btn-medium"> Pilih Materi</button></label>
+                            <select name="cars" id="cars">
+                            <option value="volvo">Volvo</option>
+                            <option value="saab">Saab</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="span3" id="third-person">
+                        <div class="thumbnail">
+                            <img src="images/mapel/bahasa-inggris.png" img="images">
+                            <h3>Bahasa Inggris</h3>
+                            <label for="cars"><button  class="btn btn-medium"> Pilih Materi</button></label>
+                            <select name="cars" id="cars">
+                            <option value="volvo">Volvo</option>
+                            <option value="saab">Saab</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="span3" id="third-person">
+                        <div class="thumbnail">
+                            <img src="images/mapel/hadits.png" img="images">
+                            <h3>Hadits</h3>
+                            <label for="cars"><button  class="btn btn-medium"> Pilih Materi</button></label>
+                            <select name="cars" id="cars">
+                            <option value="volvo">Volvo</option>
+                            <option value="saab">Saab</option>
+                            </select>
                         </div>
                     </div>
                 </div>
-            </div>
-						 
-        <!--<div class="row service-v1 margin-bottom-40">
-            <div class="col-md-4 md-margin-bottom-40">
-               <img class="img-responsive" src="img/works/1.jpg" alt="">   
-                <h3>Kelas VII</h3>
-                <p>Pembelajaran Daring Santri Kelas VII MTs Husnul Khotimah 2 Kuningan</p>        
-            </div>
-            <div class="col-md-4">
-                <img class="img-responsive" src="img/works/5.jpg" alt="">            
-                <h3>Kelas VIII</h3>
-                <p>Pembelajaran Daring Santri Kelas VIII MTs Husnul Khotimah 2 Kuningan</p>        
-            </div>
-            <div class="col-md-4 md-margin-bottom-40">
-              <img class="img-responsive" src="img/works/3.jpg" alt="">  
-                <h3>Kelas IX</h3>
-                <p>Pembelajaran Daring Santri Kelas IX MTs Husnul Khotimah 2 Kuningan</p>        
+                </br>
+                <div class="row-fluid team">
+                    <div class="span3" id="first-person">
+                        <div class="thumbnail">
+                            <img src="images/mapel/ski.png" img="images">
+                            <h3>SKI</h3>
+                            <label for="cars"><button  class="btn btn-medium"> Pilih Materi</button></label>
+                            <select name="cars" id="cars">
+                            <option value="volvo">Volvo</option>
+                            <option value="saab">Saab</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="span3" id="second-person">
+                        <div class="thumbnail">
+                            <img src="images/mapel/ips.png" img="images">
+                            <h3>IPS</h3>
+                            <label for="cars"><button  class="btn btn-medium"> Pilih Materi</button></label>
+                            <select name="cars" id="cars">
+                            <option value="volvo">Volvo</option>
+                            <option value="saab">Saab</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="span3" id="third-person">
+                        <div class="thumbnail">
+                            <img src="images/mapel/bahasa-indonesia.png" img="images">
+                            <h3>Bahasa Indonesia</h3>
+                            <label for="cars"><button  class="btn btn-medium"> Pilih Materi</button></label>
+                            <select name="cars" id="cars">
+                            <option value="volvo">Volvo</option>
+                            <option value="saab">Saab</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="span3" id="third-person">
+                        <div class="thumbnail">
+                            <img src="images/mapel/aqidah.png" img="images">
+                            <h3>Aqidah</h3>
+                            <label for="cars"><button  class="btn btn-medium"> Pilih Materi</button></label>
+                            <select name="cars" id="cars">
+                            <option value="volvo">Volvo</option>
+                            <option value="saab">Saab</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                </br>
+                <div class="row-fluid team">
+                    <div class="span3" id="first-person">
+                        <div class="thumbnail">
+                            <img src="images/mapel/pkn.png" img="images">
+                            <h3>PKn</h3>
+                            <label for="cars"><button  class="btn btn-medium"> Pilih Materi</button></label>
+                            <select name="cars" id="cars">
+                            <option value="volvo">Volvo</option>
+                            <option value="saab">Saab</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="span3" id="second-person">
+                        <div class="thumbnail">
+                            <img src="images/mapel/tsaqofah.png" img="images">
+                            <h3>Tsaqofah</h3>
+                            <label for="cars"><button  class="btn btn-medium"> Pilih Materi</button></label>
+                            <select name="cars" id="cars">
+                            <option value="volvo">Volvo</option>
+                            <option value="saab">Saab</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="span3" id="third-person">
+                        <div class="thumbnail">
+                            <img src="images/mapel/shorof.png" img="images">
+                            <h3>Shorof</h3>
+                            <label for="cars"><button  class="btn btn-medium"> Pilih Materi</button></label>
+                            <select name="cars" id="cars">
+                            <option value="volvo">Volvo</option>
+                            <option value="saab">Saab</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="span3" id="third-person">
+                        <div class="thumbnail">
+                            <img src="images/mapel/adabul-mutaalim.png" img="images">
+                            <h3>Adabul Mutaalim</h3>
+                            <label for="cars"><button  class="btn btn-medium"> Pilih Materi</button></label>
+                            <select name="cars" id="cars">
+                            <option value="volvo">Volvo</option>
+                            <option value="saab">Saab</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                </br>
+                <div class="row-fluid team">
+                    <div class="span3" id="first-person">
+                        <div class="thumbnail">
+                            <img src="images/mapel/qowaid.png" img="images">
+                            <h3>Qowaid</h3>
+                            <label for="cars"><button  class="btn btn-medium"> Pilih Materi</button></label>
+                            <select name="cars" id="cars">
+                            <option value="volvo">Volvo</option>
+                            <option value="saab">Saab</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="span3" id="first-person">
+                        <div class="thumbnail">
+                            <img src="images/mapel/tajwid.png" img="images">
+                            <h3>Tajwid</h3>
+                            <label for="cars"><button  class="btn btn-medium"> Pilih Materi</button></label>
+                            <select name="cars" id="cars">
+                            <option value="volvo">Volvo</option>
+                            <option value="saab">Saab</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>-->
             </div>
         </div>
-        <!-- End Service Blcoks -->        
-    </div>
-    </section>
+        <!-- About us section end -->
--- halaman/akun/template.php
+++ halaman/akun/template.php
@@ -1,119 +1,91 @@
-<?php ob_start(); ?>
 <!DOCTYPE html>
+<!--
+ * A Design by GraphBerry
+ * Author: GraphBerry
+ * Author URL: http://graphberry.com
+ * License: http://graphberry.com/pages/license
+-->
 <html lang="en">
-<head>
-<meta charset="utf-8">
-<title>MTs Husnul Khotimah 2 Kuningan</title>
-<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<meta name="description" content="" />
-<meta name="author" content="http://webthemez.com" />
+    
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>MTs Husnul Khotimah Kuningan</title>
+        <!-- Load Roboto font -->
+        <link href="http://fonts.googleapis.com/css?family=Roboto:400,300,700&amp;subset=latin,latin-ext" rel="stylesheet" type="text/css">
+        <!-- Load css styles -->
+        <link rel="stylesheet" type="text/css" href="asset/main/css/bootstrap.css" />
+        <link rel="stylesheet" type="text/css" href="asset/main/css/bootstrap-responsive.css" />
+        <link rel="stylesheet" type="text/css" href="asset/main/css/style.css" />
+        <link rel="stylesheet" type="text/css" href="asset/main/css/pluton.css" />
+        <!--[if IE 7]>
+            <link rel="stylesheet" type="text/css" href="css/pluton-ie7.css" />
+        <![endif]-->
+        <link rel="stylesheet" type="text/css" href="asset/main/css/jquery.cslider.css" />
+        <link rel="stylesheet" type="text/css" href="asset/main/css/jquery.bxslider.css" />
+        <link rel="stylesheet" type="text/css" href="asset/main/css/animate.css" />
+        <link rel="stylesheet" type="text/css" href="asset/main/css/embed-responsive.css" />
 
-<!-- css -->
-<link href="asset/main/css/bootstrap.min.css" rel="stylesheet" />
-<link href="asset/main/css/fancybox/jquery.fancybox.css" rel="stylesheet">
-<link href="asset/main/css/jcarousel.css" rel="stylesheet" />
-<link href="asset/main/css/flexslider.css" rel="stylesheet" />
-<link href="asset/main/js/owl-carousel/owl.carousel.css" rel="stylesheet">
-<link href="asset/main/css/style.css" rel="stylesheet" />
-<link href="asset/main/css/gallery-1.css" rel="stylesheet">
-<link href="asset/main/css/magnific-popup.css" rel="stylesheet">
-<link href="asset/main/css/embed-responsive.css" rel="stylesheet"> 
- 
-<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
-<!--[if lt IE 9]>
-      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-    <![endif]-->
-<link rel="shortcut icon" href="asset/main/img/favicon.png">
-
-</head>
-<body>
-<div id="wrapper" class="home-page">
-<div class="topbar">
-  <div class="container">
-    <div class="row">
-      <div class="col-md-12">
-        <p class="pull-left hidden-xs">MTs Husnul Khotimah 2 Kuningan</p>
-        <p class="pull-right"><i class="fa fa-phone"></i>info@mtshakadua.com</p>
-      </div>
-    </div>
-  </div>
-</div>
-	<!-- start header -->
-	<header>
-        <div class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
+        <!-- Fav and touch icons -->
+        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="asset/main/images/ico/apple-touch-icon-144.png">
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="asset/main/images/ico/apple-touch-icon-114.png">
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="asset/main/images/apple-touch-icon-72.png">
+        <link rel="apple-touch-icon-precomposed" href="images/ico/apple-touch-icon-57.png">
+        <link rel="shortcut icon" href="asset/main/images/ico/favicon.ico">
+    </head>
+    
+    <body>
+        <div class="navbar">
+            <div class="navbar-inner">
+                <div class="container">
+                    <a href="#" class="brand">
+                        <img src="asset/main/images/logo.png" width="120" height="40" alt="Logo" />
+                        <!-- This is website logo -->
+                    </a>
+                    <!-- Navigation button, visible on small resolution -->
+                    <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                        <i class="icon-menu"></i>
                     </button>
-                    <a class="navbar-brand" href="index.php"><img src="asset/main/img/logo.png" alt="logo"/>MTSHAKADUA</a>
-                </div>
-                <div class="navbar-collapse collapse ">
-                    <ul class="nav navbar-nav">
-                        <li class="<?php if(!isset($_GET['page'])) {echo "active";}?>"><a href="index.php">Home</a></li>
-                        <?php
-                            if ( $_SESSION['status'] == 2 ) {
-                                echo"";
-                            } else {
-                        ?>
-                        <li class="<?php if($_GET['page'] === "e-learning") {echo "active";}?>"><a href="user.php?page=e-learning">E-learning</a></li>
-                        <?php
-                            }
-                        ?>
-                        <li><a href="logout.php">Logout</a></li>
-                        <li><a href="index.php?page=kontak">Contact</a></li>
-                    </ul>
+                    <!-- Main navigation -->
+                    <div class="nav-collapse collapse pull-right">
+                        <ul class="nav" id="top-navigation">
+                            <li class="active"><a href="index.php">Home</a></li>
+                            <li><a href="#">Login</a></li>
+                        </ul>
+                    </div>
+                    <!-- End main navigation -->
                 </div>
             </div>
         </div>
-	</header>
-	<!-- end header
-	<section id="banner"> -->
-	 <?php require ($content); ?>
-	<footer>
-	<div id="sub-footer">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-6">
-					<div class="copyright">
-						<p>
-							<span>&copy; <a href="index.php" target="_blank">MTs Husnul Khotimah 2</a> | 2020 All right reserved. <br/> Design by</span> <a href="http://excellenz.id/" target="_blank">Excellenz.ID </a> 
-						</p>
-					</div>
-				</div>
-				<div class="col-lg-6">
-					<ul class="social-network">
-						<li><a href="#" data-placement="top" title="Facebook"><i class="fa fa-facebook"></i></a></li>
-						<li><a href="#" data-placement="top" title="Twitter"><i class="fa fa-twitter"></i></a></li>
-						<li><a href="#" data-placement="top" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
-						<li><a href="#" data-placement="top" title="Pinterest"><i class="fa fa-pinterest"></i></a></li>
-						<li><a href="#" data-placement="top" title="Google plus"><i class="fa fa-google-plus"></i></a></li>
-					</ul>
-				</div>
-			</div>
-		</div>
-	</div>
-	</footer>
-</div>
-<a href="#" class="scrollup"><i class="fa fa-angle-up active"></i></a>
-<!-- javascript
-    ================================================== -->
-<!-- Placed at the end of the document so the pages load faster -->
-<script src="asset/main/js/jquery.js"></script>
-<script src="asset/main/js/jquery.easing.1.3.js"></script>
-<script src="asset/main/js/bootstrap.min.js"></script>
-<script src="asset/main/js/jquery.fancybox.pack.js"></script>
-<script src="asset/main/js/jquery.fancybox-media.js"></script>  
-<script src="asset/main/js/jquery.flexslider.js"></script>
-<script src="asset/main/js/animate.js"></script>
-<!-- Vendor Scripts -->
-<script src="asset/main/js/modernizr.custom.js"></script>
-<script src="asset/main/js/jquery.isotope.min.js"></script>
-<script src="asset/main/js/jquery.magnific-popup.min.js"></script>
-<script src="asset/main/js/animate.js"></script>
-<script src="asset/main/js/custom.js"></script>
-<script src="asset/main/js/owl-carousel/owl.carousel.js"></script>
-</body>
+  <!-- End menu -->
+        <?php require ($content); ?>
+        <!-- Footer section start -->
+        <div class="footer">
+            <p>&copy; 2020 | All Rights Reserved | <a href="http://mtshusnulkhotimah.com">MTs Husnul Khotimah Kuningan</a> Design By : <a href="https://excellenz.id/">excellenz.id</a></p>
+        </div>
+        <!-- Footer section end -->
+        <!-- ScrollUp button start -->
+        <div class="scrollup">
+            <a href="#">
+                <i class="icon-up-open"></i>
+            </a>
+        </div>
+        <!-- ScrollUp button end -->
+        <!-- Include javascript -->
+        <script src="asset/main/js/jquery.js"></script>
+        <script type="text/javascript" src="asset/main/js/jquery.mixitup.js"></script>
+        <script type="text/javascript" src="asset/main/js/bootstrap.js"></script>
+        <script type="text/javascript" src="asset/main/js/modernizr.custom.js"></script>
+        <script type="text/javascript" src="asset/main/js/jquery.bxslider.js"></script>
+        <script type="text/javascript" src="asset/main/js/jquery.cslider.js"></script>
+        <script type="text/javascript" src="asset/main/js/jquery.placeholder.js"></script>
+        <script type="text/javascript" src="asset/main/js/jquery.inview.js"></script>
+        <!-- Load google maps api and call initializeMap function defined in app.js -->
+        <script async="" defer="" type="text/javascript" src="https://maps.googleapis.com/maps/api/js?sensor=false&callback=initializeMap"></script>
+        <!-- css3-mediaqueries.js for IE8 or older -->
+        <!--[if lt IE 9]>
+            <script src="js/respond.min.js"></script>
+        <![endif]-->
+        <script type="text/javascript" src="asset/main/js/app.js"></script>
+    </body>
 </html>
